084192e572ca40e9a3ba044e2c0dbd0d82891c29
OrangeLuis <luigimaiese@hotmail.it>
2022-04-14 11:44:47 +0200

    Aggiunta main test finali + dataset finali

---
+++ src/java/main/MainFinalTest.java
@@ -0,0 +1,79 @@
+package main;
+
+import pairHMM.customCPU.PairHMMCPU;
+import pairHMM.customGPU.PairHMMGPUCustom;
+import pairHMM.newGPU.CUDAObj;
+import pairHMM.newGPU.Dataset;
+import pairHMM.newGPU.Kernel;
+import pairHMM.newGPU.Preprocessing;
+import pairHMM.utility.Utils;
+
+import java.text.DecimalFormat;
+
+import static main.MainLoadDatasetAndCompareCustom.print_samples;
+
+public class MainFinalTest {
+
+    public static void main(String[] args) {
+        //Setting up kernel input
+        String kernelName = "src/resources/compiled_kernels/subComputationOldNoPrintsW.cubin";
+        String functionName = "subComputation";
+        Integer iterationsNumber = 1;
+        Utils.setAccuracyFormat();
+
+        //Set up output files
+
+        //Kernel obj
+        Kernel kernel = new Kernel(kernelName, functionName);
+        //CudaObj obj
+        CUDAObj cuda = new CUDAObj(kernel);
+
+        //Test 10s
+        String dataset1 = "test_data/10s.txt";
+        singleBenchmark("10s", dataset1, iterationsNumber, cuda);
+
+        //Test longer-reads
+        String dataset2 = "test_data/longer-reads.txt";
+        singleBenchmark("longer-reads", dataset2, iterationsNumber, cuda);
+
+        //Test more-samples
+        String dataset3 = "test_data/more-samples.txt";
+        singleBenchmark("more-samples", dataset3, iterationsNumber, cuda);
+
+        //Test larger-dataset
+        String dataset4 = "test_data/larger-dataset.txt";
+        singleBenchmark("larger-dataset", dataset4, iterationsNumber, cuda);
+
+    }
+
+    public static void singleBenchmark(String datasetName, String datasetPath, Integer iterationsNumber, CUDAObj cuda){
+        System.out.println("\nBenchmark" + datasetName + ": " + iterationsNumber + " iterations\n");
+
+        Dataset dataset = new Dataset();
+        dataset.readDataset(datasetPath);
+        int samples = dataset.getSamples();
+
+        Preprocessing prep = new Preprocessing(dataset);
+
+        for (int i = 0; i < iterationsNumber; i++) {
+
+            System.out.println("Iteration " + i);
+
+            PairHMMCPU cpu = new PairHMMCPU(prep, samples);
+            float[] cpuRes = cpu.calculatePairHMM();
+
+            PairHMMGPUCustom gpu = new PairHMMGPUCustom(prep, cuda);
+            float[] gpuRes = gpu.calculatePairHMM();
+
+            boolean resCheck = true;
+            for (int j = 0; j < samples; j++) {
+                DecimalFormat df = new DecimalFormat(Utils.getAccuracyFormat());
+                if (!df.format(cpuRes[j]).equals(df.format(gpuRes[j]))) {
+                    resCheck = false;
+                }
+            }
+
+            System.out.println("ResCheck " + resCheck);
+        }
+    }
+}
--- src/java/main/MainGenerateAndSaveDataset.java
+++ src/java/main/MainGenerateAndSaveDataset.java
@@ -4,10 +4,10 @@ import pairHMM.customGPU.GenerateDataset;
 
 public class MainGenerateAndSaveDataset {
     public static void main(String[] args) {
-        String filename = "test_data/longer_dataset.txt";
-        int samples = 20000;
-        int alleleLength = 128;
-        int readLength = 128;
+        String filename = "test_data/larger-dataset.txt";
+        int samples = 10000;
+        int alleleLength = 256;
+        int readLength = 256;
 
         GenerateDataset generator = new GenerateDataset(readLength, alleleLength, samples);
         generator.generate();
